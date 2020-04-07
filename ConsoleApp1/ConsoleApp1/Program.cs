8a64ca7c5bbbb6bc136f3729471382dfcb86de31
Raghvendra_Lodhi <Raghvendra_Lodhi@EPINHYDW01B1>
2020-04-07 12:07:39 +0530

    com 1 in b2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! raghvendra");
+            Console.WriteLine("welcome to hyd");
         }
     }
 }
